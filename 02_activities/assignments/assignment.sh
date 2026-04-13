aa990ed770c6d86cddb0dae47fe8345d4602c451
Jake Ford <jake@solstice.us>
2026-04-13 15:17:58 -0400

    v2

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -35,8 +35,7 @@ mkdir data
 mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
-
-ls ./data/raw
+ls data/raw
 
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
@@ -51,8 +50,8 @@ cp ./data/raw/*user*.log ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm -f ./data/raw/*ipaddr*.log
-rm -f ./data/processed/user_logs/*ipaddr*.log
+rm -f ./data/raw/*ipaddr*
+rm -f ./data/processed/user_logs/*ipaddr*
 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
